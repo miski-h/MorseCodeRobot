08440f634537767c5a553f57686d091ffff898b2
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-02-29 15:07:08 +0000

    Update Driver.java

--- FinalProject/src/Driver.java
+++ FinalProject/src/Driver.java
@@ -1,24 +1,35 @@
-import lejos.hardware.Battery;
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.hardware.port.MotorPort;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.navigation.MovePilot;
 import lejos.hardware.port.SensorPort;
-import lejos.hardware.motor.BaseRegulatedMotor;
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
-import lejos.robotics.SampleProvider;
 
-
-public class Driver{
+public class Driver {
     public static void main(String[] args) {
-    	System.out.println("Welcome to Our Robot Program!");
+
+        System.out.println("Welcome to Our Robot Program!");
     	System.out.println("Authors: Samuel Haddock, Yash Kumar and Miski Hussein");
     	System.out.println("Version: 1.0");
 
     	System.out.println("Press any button to continue...");
     	Button.waitForAnyPress();
-    	    
+        
+        // Initialize MovePilot for driving
+        MovePilot pilot = new MovePilot(5.6f, 12.0f, Motor.A, Motor.B);
+
+        // Initialize behaviors
+        Behavior forwardBehavior = new ForwardBehavior(pilot);
+        Behavior avoidWallBehavior = new AvoidWallBehavior(SensorPort.S1, pilot);
+        Behavior lowBatteryBehavior = new LowBatteryBehavior();
+
+        // Construct an array of behaviors
+        Behavior[] behaviors = {forwardBehavior, avoidWallBehavior, lowBatteryBehavior};
+
+        // Initialize an arbitrator with the behaviors
+        Arbitrator arbitrator = new Arbitrator(behaviors);
+
+        // Start the arbitrator
+        arbitrator.go();
     }
 }
 

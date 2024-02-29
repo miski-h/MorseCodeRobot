eb0cfa3e8907fc3b7e7aa2d7166822f38cb341a2
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-02-29 14:58:35 +0000

    Update Battery.java

--- FinalProject/src/Battery.java
+++ FinalProject/src/Battery.java
@@ -1,1 +1,35 @@
 
+import lejos.hardware.Battery;
+import lejos.robotics.subsumption.Behavior;
+import lejos.hardware.Sound;
+
+public class Battery implements Behavior {
+    private boolean suppressed = false;
+
+    // Returns true if the battery level is low
+    public boolean takeControl() {
+        return Battery.getVoltage() < 6.0; // You may need to adjust this threshold based on your battery and system setup
+    }
+
+    // Just notifies the action method by setting a field to true
+    public void suppress() {
+        suppressed = true;
+    }
+
+    // Flash the battery low message and beep
+    public void action() {
+        suppressed = false;
+        // Flash battery low message
+        System.out.println("Battery low!");
+        // Beep to alert user
+        Sound.beep();
+        // Wait a bit for the message to be visible and the beep to be heard
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Exit the program
+        System.exit(0);
+    }
+}

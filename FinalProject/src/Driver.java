19bcdc58ab8c7087d13b7c9f8c07d168367860dc
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-02-29 16:39:15 +0000

    Update Driver.java

--- FinalProject/src/Driver.java
+++ FinalProject/src/Driver.java
@@ -26,13 +26,17 @@ public class Driver {
 
         Arbitrator arbitrator = new Arbitrator(behaviors);
         
-        while(true) {
-        	if (Button.ENTER.isDown()){
-        		arbitrator.stop();
-        		break;
-        	}
-        	arbitrator.go();
-        }     
+        Button.ESCAPE.addButtonListener(new ButtonListener() {
+            public void buttonPressed(Button b) {
+                System.exit(0);
+            }
+
+            public void buttonReleased(Button b) {
+                // Do nothing on release
+            }
+        });
+
+	arbitrator.go()  
     }
 }
 

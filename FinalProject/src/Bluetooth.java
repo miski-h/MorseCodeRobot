5bb68c6188d2fda7d341d931a7bcdf12b72a3626
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-02-29 16:55:46 +0000

    Update Bluetooth.java

--- FinalProject/src/Bluetooth.java
+++ FinalProject/src/Bluetooth.java
@@ -1,13 +1,33 @@
+import lejos.hardware.Bluetooth;
+import lejos.robotics.subsumption.Behavior;
 
-public class Bluetooth {
+public class Bluetooth implements Behavior {
+    private boolean suppressed = false;
+    private boolean bluetoothMessageAvailable = false;
 
-	public Bluetooth() {
-		// TODO Auto-generated constructor stub
-	}
+    // Returns true if a Bluetooth message is available
+    public boolean takeControl() {
+        return Bluetooth.getNXTCommConnector().available() > 0;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Just notifies the action method by setting a field to true
+    public void suppress() {
+        suppressed = true;
+    }
 
-	}
+    // Sets a state variable for further action after Bluetooth behavior completes
+    public void action() {
+        suppressed = false;
+        bluetoothMessageAvailable = true;
+    }
 
+    // Method to check if a Bluetooth message is available
+    public boolean isBluetoothMessageAvailable() {
+        return bluetoothMessageAvailable;
+    }
+
+    // Method to reset the state of Bluetooth message availability
+    public void resetBluetoothMessageAvailability() {
+        bluetoothMessageAvailable = false;
+    }
 }

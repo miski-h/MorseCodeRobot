b31884515466c86c8f895731d69f6d2eb0a0eba9
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-02-29 14:54:35 +0000

    Update Trundle.java

--- FinalProject/src/Trundle.java
+++ FinalProject/src/Trundle.java
@@ -1,9 +1,34 @@
+import lejos.robotics.subsumption.Behavior;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.navigation.MovePilot;
 
-public class Trundle {
+public class Trundle implements Behavior {
+    private boolean suppressed = false;
+    private MovePilot pilot;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public ForwardBehavior(MovePilot pilot) {
+        this.pilot = pilot;
+    }
 
-	}
+    // Always returns true to indicate this behaviour should take control.
+    public boolean takeControl() {
+        return true;
+    }
 
+    // Makes the MovePilot go forward and then stops it.
+    public void action() {
+        suppressed = false;
+        // Move forward
+        pilot.forward();
+        while (!suppressed) {
+            Thread.yield();
+        }
+        // Stop the pilot
+        pilot.stop();
+    }
+
+    // Notifies the action method to exit.
+    public void suppress() {
+        suppressed = true;
+    }
 }

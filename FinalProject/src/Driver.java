87587fb8949ec883196b13a0d22fdf08b37b9b8c
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-03-12 16:00:30 +0000

    Added commands to the driver.java file

--- FinalProject/src/Driver.java
+++ FinalProject/src/Driver.java
@@ -1,13 +1,19 @@
+import lejos.hardware.Button;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.Motor;
-import lejos.robotics.SampleProvider;
-import lejos.robotics.navigation.MovePilot;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.localization.PoseProvider;
+import lejos.robotics.navigation.MovePilot;
+import lejos.hardware.sensor.NXTSoundSensor;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
-import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.sensor.NXTSoundSensor;
-
 
 public class Driver {
     public static void main(String[] args) {
@@ -16,47 +22,76 @@ public class Driver {
         System.out.println("Version: 1.0");
         System.out.println("Press Enter button to continue...");
         Button.ENTER.waitForPress();
-        
-        LCD.clear();
-        /*
-        @SuppressWarnings("deprecation")
-        MovePilot pilot = new MovePilot(5.6f, 12.0f, Motor.A, Motor.B);
 
+        BaseRegulatedMotor mL = new EV3LargeRegulatedMotor(MotorPort.A);
+        BaseRegulatedMotor mR = new EV3LargeRegulatedMotor(MotorPort.B);
+
+        LCD.clear();
+        @SuppressWarnings("deprecation")
+		MovePilot pilot = new MovePilot(5.6f, 12.0f, Motor.A, Motor.B); // Fixed motor references
 
         // Initialise the sound sensor
         NXTSoundSensor soundSensor = new NXTSoundSensor(SensorPort.S2);
         SampleProvider soundMode = soundSensor.getDBAMode();
 
-
-        // Create behaviour to detect sound input
-        SoundDetection soundDetectionBehavior = new SoundDetection(soundMode);
         Behavior forwardBehavior = new Trundle(pilot);
         Behavior avoidWallBehavior = new Backup(SensorPort.S3, pilot);
         Behavior lowBatteryBehavior = new LowBattery();
         Behavior darkChecker = new DarkChecker(pilot, SensorPort.S1);
-
-        // Set the program to be running
-        while (!Button.ENTER.isDown()) {
-        	soundDetectionBehavior.setRunning(true);
-        }
-        
-        Behavior[] behaviors = {avoidWallBehavior, lowBatteryBehavior, darkChecker, soundDetectionBehavior};
+        Behavior[] behaviors = {avoidWallBehavior, lowBatteryBehavior, darkChecker};
         Arbitrator arbitrator = new Arbitrator(behaviors);
-
 
         while (!Button.ENTER.isDown()) {
             arbitrator.go();
+            squareCommand(mL, mR); // Fixed method call
         }
-
 
         // Stop the arbitrator and close the sound sensor when the enter button is pressed
         arbitrator.stop();
         soundSensor.close();
-        */
+    }
+
+    public static void squareCommand(BaseRegulatedMotor mL, BaseRegulatedMotor mR) { // Fixed method name
+        final float WHEEL_DIAMETER = 56;
+        final float AXLE_LENGTH = 44;
+        final float ANGULAR_SPEED = 200;
+        final float LINEAR_SPEED = 200;
+
+        Wheel wLeft = WheeledChassis.modelWheel(mL, WHEEL_DIAMETER).offset(-AXLE_LENGTH / 2);
+        Wheel wRight = WheeledChassis.modelWheel(mR, WHEEL_DIAMETER).offset(AXLE_LENGTH / 2);
+
+        Wheel[] wheels = new Wheel[] {wRight, wLeft}; // Corrected the order of wheels
+
+        WheeledChassis chassis = new WheeledChassis(wheels, WheeledChassis.TYPE_DIFFERENTIAL);
+        MovePilot pilot = new MovePilot(chassis);
+
+        // Using OdometryPoseProvider
+        PoseProvider poseProvider = new OdometryPoseProvider(pilot);
+
+        for (int side = 0; side < 4; side++) {
+            pilot.setAngularSpeed(ANGULAR_SPEED);
+            pilot.setLinearSpeed(LINEAR_SPEED);
+            pilot.travel(300);
+            pilot.rotate(90); // Changed the angle to make a square
+        }
+
+        // Close motor ports to release resources
+        mL.close();
+        mR.close();
+    }
+
+    public void circleCommand() {
+
+    }
+
+    public void freeRoamCommand() {
+
+    }
+
+    public void danceCommand() {
+
     }
 }
-
-
 
 
 
--- FinalProject/src/TestMorseCode.java
+++ FinalProject/src/TestMorseCode.java
@@ -6,45 +6,49 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.NXTSoundSensor;
 import lejos.hardware.sensor.SensorMode;
 
-
 public class TestMorseCode {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A);
         BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.B);
 
-
         NXTSoundSensor ss = new NXTSoundSensor(SensorPort.S2);
         SensorMode sound = (SensorMode) ss.getDBAMode();
-        SoundDetection clap = new SoundDetection(sound, 0.4f, 200, 600);
-
+        SoundDetection clap = new SoundDetection(sound, 0.6f, 200, 500);
 
         float[] level = new float[1];
 
-
-        int i = 0;
-
+        boolean dashInProgress = false; // Track whether a dash is in progress
         System.out.print("Morse Code Heard:");
-        while (i == 0) {
+        while (true) {
             if (Button.ENTER.isDown()) {
-                i++;
                 break;
             }
-            
+
             clap.fetchSample(level, 0);
             if (level[0] == 1.0) {
-                System.out.println("Dot heard");
-            } else if (level[0] == 2.0) {
+                // Single clap (potential dot)
+                Thread.sleep(300); // Wait for potential second clap within dash time gap
+                clap.fetchSample(level, 0); // Fetch sample again
+                if (level[0] == 2.0) {
+                    System.out.println("Dash heard");
+                    dashInProgress = true; // Set dash in progress when a dash is detected
+                } else {
+                    System.out.println("Dot heard");
+                    dashInProgress = false; // Reset dash in progress
+                }
+            } else if (level[0] == 2.0 && !dashInProgress) {
+                // Only detect dash if not already in progress
                 System.out.println("Dash heard");
+                dashInProgress = true; // Set dash in progress when a dash is detected
+            } else {
+                dashInProgress = false; // Reset dash in progress if no clap is detected
             }
         }
-
 
         mLeft.close();
         mRight.close();
     }
 }
-
-
 
 
 

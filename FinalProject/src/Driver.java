3a32dbc5c8d20fa46ad9da116c13ab42f8ac7450
Sammy-Haddock <156518948+Sammy-Haddock@users.noreply.github.com>
2024-03-13 15:53:45 +0000

    Added Delay Import

--- FinalProject/src/Driver.java
+++ FinalProject/src/Driver.java
@@ -14,6 +14,7 @@ import lejos.robotics.navigation.MovePilot;
 import lejos.hardware.sensor.NXTSoundSensor;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
 public class Driver {
     public static void main(String[] args) {
@@ -81,6 +82,7 @@ public class Driver {
     }
 
     public static void circleCommand(BaseRegulatedMotor mL, BaseRegulatedMotor mR) {
+    	System.out.print("Running circle command...");
     	mR.rotate(360);
         mL.forward(); Delay.msDelay(1000); 
         mL.stop(); Delay.msDelay(200); 
